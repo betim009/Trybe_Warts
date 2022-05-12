bf0f9aa0db9b02ef79a835b6a26a9c33352a79f5
Cauê Mustafá <cauemustafa@live.com>
2022-05-12 13:13:35 -0300

    Ajustando funções no script.js

--- script.js
+++ script.js
@@ -1,42 +1,35 @@
-function verificandoInputs() {
+function logInVerifier() {
   const getButton = document.querySelector('#button-submit');
+
   getButton.addEventListener('click', () => {
     const getEmail = document.querySelector('#log-email').value;
     const getPassword = document.querySelector('#password').value;
-    if (getEmail === 'tryber@teste.com' && getPassword === '123456') {
-      alert('Olá, Tryber!');
-    } else {
-      alert('Email ou senha inválidos.');
-    }
-  });
-}
-verificandoInputs();
 
-function contadorCaractere() {
+    if (getEmail === 'tryber@teste.com' && getPassword === '123456') alert('Olá, Tryber!');
+    else alert('Email ou senha inválidos.');
+  });
+} logInVerifier();
+
+function charactersCount() {
   const textArea = document.querySelector('#textarea');
   const count = document.querySelector('#counter');
+
   let tamanhoTextArea = textArea.value.length;
 
   textArea.addEventListener('input', () => {
     tamanhoTextArea = textArea.value.length;
     count.innerHTML = `Restam ${500 - tamanhoTextArea} caracteres`;
   });
-}
-contadorCaractere();
+} charactersCount();
 
-function desactiveSubmit() {
+function submitBtnDesactive() {
   const agreement = document.querySelector('#agreement');
   const submit = document.querySelector('#submit-btn');
+
   submit.disabled = true;
 
   agreement.addEventListener('change', () => {
-    if (submit.disabled === true) {
-      submit.disabled = false;
-    } else {
-      submit.disabled = true;
-    }
+    if (submit.disabled === true) submit.disabled = false;
+    else submit.disabled = true;
   });
-
-  // agreement.addEventListener('change', () => (submit.disabled === true ? submit.disabled = false : submit.disabled = true));
-}
-desactiveSubmit();
+} submitBtnDesactive();
